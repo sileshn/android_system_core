495ed4bb3f77cf05569471f725723103fc094e14
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-22 12:09:57 +0800

    Spoof encrytion prop as encrypted
    * reference: https://github.com/RisingOS-staging/android_vendor_rising/commit/deae9373dcd0d93f09548ee5ef53a517e7cad2f4
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1463,6 +1463,7 @@ static void SetSafetyNetProps() {
     InitPropertySet("vendor.boot.verifiedbootstate", "green");
     InitPropertySet("oplusboot.verifiedbootstate", "green");
     InitPropertySet("sys.oem_unlock_allowed", "0");
+    InitPropertySet("ro.crypto.state", "encrypted");
 }
 
 void PropertyInit() {
